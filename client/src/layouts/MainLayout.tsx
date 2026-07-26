ceedde140f9acbef531c8d9a071d4060071d18b9
Matt <141237843+FabianDev033@users.noreply.github.com>
2026-07-26 00:28:33 -0300

    feat: Added scrollToTop to improve UX

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import {Header, Footer} from "../Components/common";
+import { ScrollToTop } from "../Components/mainLayout";
 export default function MainLayout() {
   return (
     <>
     <div className="flex min-h-svh flex-col">
+      <ScrollToTop/>
       <Header/>
 
       <main className="flex-1">
