fb30103b53381b3018ca732d534350b9ff23c722
Matt <141237843+FabianDev033@users.noreply.github.com>
2026-08-15 11:54:35 -0300

    refactor: refactored menu items

--- client/src/Components/common/Menu.tsx
+++ client/src/Components/common/Menu.tsx
@@ -13,55 +13,66 @@ export default function Menu() {
   const { pathname } = useLocation();
   if (pathname === "/admin/login") return null;
 
+  const menu = [
+    {
+      name: "Dashboard",
+      icon: <Dashboard className="w-7 aspect-square" />,
+      path: "/admin",
+      navigate: () => {
+        navigate("/admin");
+      },
+    },
+    {
+      name: "Ordenes",
+      icon: <Orders className="w-7 aspect-square" />,
+      path: "/admin/ordenes",
+      navigate: () => {
+        navigate("ordenes");
+      },
+    },
+    {
+      name: "Productos",
+      icon: <Products className="w-7 aspect-square" />,
+      path: "/admin/productos",
+      navigate: () => {
+        navigate("productos");
+      },
+    },
+    {
+      name: "Agregar Producto",
+      icon: <AddProduct className="w-7 aspect-square" />,
+      path: "/admin/agregarProducto",
+      navigate: () => {
+        navigate("agregarProducto");
+      },
+    },
+    {
+      name: "Actualizar Stock",
+      icon: <AddStock className="w-7 aspect-square" />,
+      path: "/admin/actualizarStock",
+      navigate: () => {
+        navigate("actualizarStock");
+      },
+    },
+  ];
   return (
-    <aside className="w-80 absolute left-0 h-svh bg-bg-dark flex flex-col items-center gap-15">
+    <aside className="w-80 h-svh bg-bg-dark flex flex-col items-center gap-15">
       <section className="w-11/12 flex flex-col gap-5 mt-3">
         <img src={Logo} alt="" className="w-20 aspect-square" />
-        <span className="font-Outfit font-medium text-4xl text-black text-shadow-2xs">
+        <span className="font-Outfit font-medium text-3xl text-black text-shadow-2xs">
           Entre Hojas
         </span>
       </section>
-      <section className="w-full px-1 flex flex-col items-center gap-10 text-black font-Outfit font-normal text-xl">
-        <div
-          className={`flex gap-3 items-center cursor-pointer w-full px-3 py-2 transition-all ease-in-out duration-500 ${pathname === "/admin" ? "bg-bg-light rounded-md shadow-md text-alt" : null}`}
-          onClick={() => {
-            navigate("/admin");
-          }}>
-          <Dashboard className="w-8 aspect-square" />
-          <span className="text-black">Dashboard</span>
-        </div>
-        <div
-          className={`flex gap-3 items-center cursor-pointer w-full px-3 py-2 transition-all ease-in-out duration-500 ${pathname === "/admin/ordenes" ? "bg-bg-light rounded-md shadow-md text-alt" : null}`}
-          onClick={() => {
-            navigate("ordenes");
-          }}>
-          <Orders className="w-8 aspect-square" />
-          <span className="text-black">Ordenes</span>
-        </div>
-        <div
-          className={`flex gap-3 items-center cursor-pointer w-full px-3 py-2 transition-all ease-in-out duration-500 ${pathname === "/admin/productos" ? "bg-bg-light rounded-md shadow-md text-alt" : null}`}
-          onClick={() => {
-            navigate("productos");
-          }}>
-          <Products className="w-8 aspect-square" />
-          <span className="text-black">Productos</span>
-        </div>
-        <div
-          className={`flex gap-3 items-center cursor-pointer w-full px-3 py-2 transition-all ease-in-out duration-500 ${pathname === "/admin/agregarProducto" ? "bg-bg-light rounded-md shadow-md text-alt" : null}`}
-          onClick={() => {
-            navigate("agregarProducto");
-          }}>
-          <AddProduct className="w-8 aspect-square" />
-          <span className="text-black">Agregar Producto</span>
-        </div>
-        <div
-          className={`flex gap-3 items-center cursor-pointer w-full px-3 py-2 transition-all ease-in-out duration-500 ${pathname === "/admin/actualizarStock" ? "bg-bg-light rounded-md shadow-md text-alt" : null}`}
-          onClick={() => {
-            navigate("actualizarStock");
-          }}>
-          <AddStock className="w-8 aspect-square" />
-          <span className="text-black">Actualizar Stock</span>
-        </div>
+      <section className="w-full px-1 flex flex-col items-center gap-8 text-black font-Outfit font-normal text-lg">
+        {menu.map((item, index) => (
+          <div
+            key={index}
+            className={`flex gap-3 items-center cursor-pointer w-full px-3 py-2 transition-all ease-in-out duration-300 ${pathname === item.path ? "bg-bg-light rounded-md shadow-md text-alt" : null}`}
+            onClick={item.navigate}>
+            {item.icon}
+            <span className="text-black">{item.name}</span>
+          </div>
+        ))}
       </section>
     </aside>
   );
